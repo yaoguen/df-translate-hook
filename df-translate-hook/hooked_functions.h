2802c5e0fdb58429f2d4b1df0d05a1d707c2964d
aNNiPAk <annipak@yandex.ru>
2020-05-24 17:40:59 +0300

    Set prototype function strncpy

--- df-translate-hook/hooked_functions.h
+++ df-translate-hook/hooked_functions.h
@@ -1,3 +1,13 @@
 #pragma once
+#include <Windows.h>
+#include "hook_helper.h"
 
+typedef char* (__cdecl* strncpyP)(char* Dest, char* Source, size_t Count);
+
+// Set address function from df.exe
+#if defined _M_IX86
+	SETUP_ORIG_FUNC(strncpyP, 0xC780);  //FIXME: set actual address function in x32 process
+#elif defined _M_X64
+	SETUP_ORIG_FUNC(strncpyP, 0xC780);
+#endif // _M_X64
 

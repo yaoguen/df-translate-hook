03e292487c426542e64386c486b96cb564c4d5c2
aNNiPAk <annipak@yandex.ru>
2020-05-24 23:01:44 +0300

    Fixed the strncpyP function for x32 and x64

--- df-translate-hook/hooked_functions.cpp
+++ df-translate-hook/hooked_functions.cpp
@@ -3,19 +3,32 @@
 
 // Set address function from df.exe
 #if defined _M_IX86
-SETUP_ORIG_FUNC(strncpyP, 0xC780);  //FIXME: set actual address function in x32 process
+	SETUP_ORIG_FUNC(strncpyP, 0xa130);
 #elif defined _M_X64
-SETUP_ORIG_FUNC(strncpyP, 0xC780);
-#endif // _M_X64
+	SETUP_ORIG_FUNC(strncpyP, 0xC780);
+#endif // _M_IX86
 
+
+#if defined _M_IX86
+char* __fastcall h(strncpyP)(void* Dest, DWORD EDX, char* Source, int Count)
+{
+	if (DictSearch(Source)) {
+		Count = strlen(Source);
+		printf("%s\n", Source);
+	}
+	
+	return o(strncpyP)(Dest, EDX, Source, Count);
+}
+#elif defined _M_X64
 char* __cdecl h(strncpyP)(char* Dest, char* Source, int Count)
 {
 	if (DictSearch(Source)) {
 		Count = strlen(Source);
+		printf("%s\n", Source);
 	}
-
 	return o(strncpyP)(Dest, Source, Count);
 }
+#endif // _M_IX86
 
 
 void AttachFunctions()
--- df-translate-hook/hooked_functions.h
+++ df-translate-hook/hooked_functions.h
@@ -2,6 +2,10 @@
 #include "detours.h"
 #include "hook_helper.h"
 
+#ifdef _M_IX86
+typedef char* (__fastcall* strncpyP)(void* Dest, DWORD EDX, char* Source, int Count);
+#elif defined _M_X64
 typedef char* (__cdecl* strncpyP)(char* Dest, char* Source, size_t Count);
+#endif // _M_IX86
 
 void AttachFunctions();

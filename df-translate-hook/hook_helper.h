6ab4c8c5498b0c547750a6fbb94f9ec3ed17c5e5
aNNiPAk <annipak@yandex.ru>
2020-05-24 17:32:08 +0300

    Added defines for help

--- df-translate-hook/hook_helper.h
+++ df-translate-hook/hook_helper.h
@@ -1,3 +1,10 @@
 #pragma once
 
+#define SETUP_ORIG_FUNC(nameFunc, shift) \
+	nameFunc nameFunc##_orig = (nameFunc)((UINT64)GetModuleHandle(0) + shift);
 
+#define ATTACH(nameFunc) \
+	DetourAttach(&(PVOID&)(nameFunc##_orig), (PVOID)nameFunc##_hook)
+
+#define h(nameFunc) nameFunc##_hook
+#define o(nameFunc) nameFunc##_orig

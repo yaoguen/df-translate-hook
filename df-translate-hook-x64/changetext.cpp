b302e351ccf318809fb84d8f74f3ba8c0ba3d608
mmanturov <mmanturov@medsoft.su>
2022-06-16 16:17:40 +0300

    Remove unused global buffer in changetext.cpp

--- df-translate-hook-x64/changetext.cpp
+++ df-translate-hook-x64/changetext.cpp
@@ -33,9 +33,6 @@ EXPORT int Init() {
     return pfuncChangeText!= nullptr;
 }
 
-#define BUFFER_SIZE 0x100
-uint16_t buffer[BUFFER_SIZE];
-
 size_t my_strlen(uint16_t * s) {
     size_t i;
     for(i=0; s[i]; i++);
